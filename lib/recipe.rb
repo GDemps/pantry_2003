1ca831b257a50b2b6bbf293607c115bd2dd91f35
Grant Dempsey <CryptoDemps@gmail.com>
2020-04-22 11:01:30 -0600

    final commit

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -8,4 +8,12 @@ class Recipe
     @ingredients_required = {}
   end
 
+  def add_ingredient(ingredient, amount)
+    if @ingredients_required[ingredient] == nil
+      ingredients_required[ingredient] = amount
+    else
+      ingredients_required[ingredient] + amount = amount
+    end
+  end
+
 end
--- test/test_recipe.rb
+++ test/test_recipe.rb
@@ -15,4 +15,14 @@ class RecipeTest < Minitest::Test
     recipe1 = Recipe.new("Mac and Cheese")
     assert_equal ({}), recipe1.ingredients_required
   end
+
+  def test_add_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    assert_equal 2, recipe1.add_ingredient(ingredient1, 2)
+    assert_equal 6, recipe1.add_ingredient(ingredient1, 4)
+    assert_equal 8, recipe1.add_ingredient(ingredient2, 8)
+
+  end
 end
