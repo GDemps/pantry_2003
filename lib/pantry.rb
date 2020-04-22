1e28eb4eeedee3a0bbe5752045a7ee9d3a446ea1
Grant Dempsey <CryptoDemps@gmail.com>
2020-04-22 09:52:23 -0600

    stock check testing complete

---
+++ lib/pantry.rb
@@ -0,0 +1,22 @@
+class Pantry
+
+  attr_reader :stock
+
+  def initialize(stock = {})
+    @stock = stock
+  end
+
+  def stock_check(ingredient)
+      if stock[ingredient] == nil
+        stock[ingredient] = 0
+      else
+        stock[ingredient]
+      end
+  end
+
+  def restock
+
+  end
+
+
+end
---
+++ test/test_stock.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class StockTest < Minitest::Test
+
+  def test_pantry_stock
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
+
+end
